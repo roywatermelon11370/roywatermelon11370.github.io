26334b36f39ad56c4c20b80cf755733f09da3a9d
roywatermelon11370 <53699625+roywatermelon11370@users.noreply.github.com>
2020-02-09 15:38:28 +0800

    ajaxify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,75 @@
 $(document).ready(function () {
-    $("#filelist > a").click(function (event) {
-        event.preventDefault();
-        var tet = $(this).attr('value');
-        $('#home').attr('href', 'filelist.html');
-        $('#breadChild').text(tet);
-        $("#content").load(this.href);
-    });
+    jQuery('#content,#nav,#breadcrumb').ajaxify();
+    // // var nowGet = location.search;
+    // // if (nowGet === '?example2.html') {
+    // //     $("#content").load('example2.html');
+    // //     $('#breadChild').text('example2.html');
+    // // }
 
-    $("#home").click(function (event) {
-        var home = $('#home').attr('href');
-        if (typeof home !== typeof undefined && home !== false) {
-            event.preventDefault();
-            $('#breadChild').text('');
-            $("#content").load(this.href);
-            $('#home').removeAttr('href');
-        }
-    });
+    // // if (nowGet === '?example3.html') {
+    // //     $("#content").load('example3.html');
+    // //     $('#breadChild').text('example3.html');
+    // // }
+
+    // // if (nowGet === '?example4.html') {
+    // //     $("#content").load('example4.html');
+    // //     $('#breadChild').text('example4.html');
+    // // }
+
+    // // if (nowGet === '?help.html') {
+    // //     $("#content").load('help.html');
+    // //     $('#breadChild').text('help.html');
+    // // }
+
+    // // if (nowGet === '?') {
+    // //     $("#content").load('filelist.html');
+    // // }
+
+
+    
+
+    // // $("#filelist > a").click(function (event) {
+    // //     event.preventDefault();
+    // //     var tet = $(this).attr('value');
+    // //     $('#home').attr('href', 'filelist.html');
+    // //     $('#breadChild').text(tet);
+    // //     $("#content").load(tet);
+    // //     history.pushState({ page: 1 }, "title 1", this.href);
+    // // });
+
+    // // $("#home").click(function (event) {
+    // //     var home = $('#home').attr('href');
+    // //     if (typeof home !== typeof undefined && home !== false) {
+    // //         event.preventDefault();
+    // //         $('#breadChild').text('');
+    // //         $("#content").load('filelist.html');
+    // //         $('#home').removeAttr('href');
+    // //         history.replaceState({ page: 2 }, "title 2", "?");
+    // //     }
+    // });
+
+    // window.onpopstate = function (event) {
+    //     var nowGet = location.search;
+    //     if (nowGet === '?example2.html') {
+    //         $("#content").load('example2.html');
+    //     }
+
+    //     if (nowGet === '?example3.html') {
+    //         $("#content").load('example3.html');
+    //     }
+
+    //     if (nowGet === '?example4.html') {
+    //         $("#content").load('example4.html');
+    //     }
+
+    //     if (nowGet === '?help.html') {
+    //         $("#content").load('help.html');
+    //     }
+
+    //     if (typeof nowGet === typeof undefined || nowGet === false || nowGet === '' || nowGet === '?') {
+    //         $("#content").load('filelist.html');
+    //     }
+    // });
 });
 
 
