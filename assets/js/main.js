9ee7418d005d62e8f106dbcebcc90d7055664b70
roywatermelon11370 <53699625+roywatermelon11370@users.noreply.github.com>
2020-02-10 15:08:06 +0800

    ggg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
     $('#content,#nav,#breadcrumb').ajaxify();
 
-    $(window).on("pronto.request", function() {
-        $('#loading').css('display','flex');
+    $(window).on("pronto.request", function () {
+        $('#loading').css('display', 'flex');
     });
 
-    $(window).on("pronto.render", function() {
-        $('#loading').css('display','none');
+    $(window).on("pronto.render", function () {
+        $('#loading').css('display', 'none');
     });
 
-   chbg = function() {
-       $('#content').toggleClass('bg-beer');
-   }
-    
+    chbg = function () {
+        $('#content').addClass('bg-beer');
+        $('li,h5,h6').text('7122');
+        $('.btn-transparent').remove();
+        $('#egg').load('egg.html');
+    }
+
+    $('#submit').on('click',function() {
+        var val = $('#number').val();
+        if(val == '99747') {
+            $('#number').addClass('is-valid').removeClass('is-invalid');
+        } else {
+            $('#number').addClass('is-invalid');
+        }
+    });
+
 });
 
 
